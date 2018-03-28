6d995fd46f4e3cb30489a5384dfad7de52107e24
Corey <coreymac193@hotmail.com>
2018-03-28 15:23:04 +1100

    Added Load + Capture Image

--- app/src/main/java/com/example/u3182551/itemrecognition/NewIRCapture.java
+++ app/src/main/java/com/example/u3182551/itemrecognition/NewIRCapture.java
@@ -1,9 +1,12 @@
 package com.example.u3182551.itemrecognition;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -23,15 +26,35 @@ import java.io.InputStream;
 
 
 public class NewIRCapture extends AppCompatActivity {
+    private Uri mImageUri;
+    private Bitmap mBitmap;
+    private static final int REQUEST_TAKE_PHOTO = 0;
+    private static final int REQUEST_SELECT_IMAGE_IN_ALBUM = 1;
 
     public VisionServiceClient visionServiceClient = new VisionServiceRestClient("1caf0086471f49d08693b33a8d3e261b", "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0");
+
+    public void capture(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+
+        }
+    }
+
+    public void load(View view) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_SELECT_IMAGE_IN_ALBUM);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_ircapture);
 
-        Bitmap mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.placeholder);
+        Bitmap mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.cattest);
         ImageView imageView = findViewById(R.id.imageView);
 
         Button btnProcess = findViewById(R.id.btnProcess);
@@ -74,17 +97,15 @@ public class NewIRCapture extends AppCompatActivity {
                     @Override
                     protected void onPostExecute(String s) {
                         mDialog.dismiss();
-                        StringBuilder stringBuilder = new StringBuilder();
-                        TextView textView = findViewById(R.id.txtDescription);
-                        try {
-                            AnalysisResult result = new Gson().fromJson(s, AnalysisResult.class);
-                            for (Caption caption:result.description.captions) {
-                                stringBuilder.append(caption.text);
-                            }
-                        } catch(Exception e) {
-                            textView.setText(stringBuilder);
-                        }
 
+                        AnalysisResult result = new Gson().fromJson(s, AnalysisResult.class);
+                        TextView textView = findViewById(R.id.txtDescription);
+                        StringBuilder stringBuilder = new StringBuilder();
+
+                        for (Caption caption : result.description.captions) {
+                            stringBuilder.append(caption.text);
+                        }
+                        textView.setText(stringBuilder);
                     }
 
                     @Override
@@ -96,5 +117,28 @@ public class NewIRCapture extends AppCompatActivity {
                 visionTask.execute(inputStream);
             }
         });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            mBitmap = (Bitmap) extras.get("data");
+        }
+        if (requestCode == REQUEST_SELECT_IMAGE_IN_ALBUM && resultCode == RESULT_OK){
+            // If image is selected successfully, set the image URI and bitmap.
+            mImageUri = data.getData();
+            mBitmap = ImageHelper.loadSizeLimitedBitmapFromUri(
+                    mImageUri, getContentResolver());
+        }
+
+        if (mBitmap != null) {
+            // Show the image on screen.
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageBitmap(mBitmap);
+
+            // runOCR();
+        }
     }
 }
